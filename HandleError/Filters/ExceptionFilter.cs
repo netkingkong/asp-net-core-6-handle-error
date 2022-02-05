778dc7c0a549c6faea56a7e6555058b2fe77e260
Richard Zheng <toair3723@hotmail.com>
2022-02-05 11:19:38 +0800

    Initial Create

---
+++ HandleError/Filters/ExceptionFilter.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace HandleError.Filters
+{
+    public class ExceptionFilter : IActionFilter, IOrderedFilter
+    {
+        public int Order { get; } = int.MaxValue - 10;
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if(context.Exception != null)
+            {
+                var exception = context.Exception;
+                var exceptionType = exception.GetType();
+                var exceptionDetails = exception.ToString();
+                HttpStatusCode status = HttpStatusCode.InternalServerError;
+                var message = exceptionDetails.ToString();
+                if(exceptionType== typeof(UnauthorizedAccessException))
+                {
+                    message = "Access to the web api is not authorized";
+                    status = HttpStatusCode.Unauthorized;
+                }else if(exceptionType== typeof(HttpRequestException))
+                {
+                    message = "Internal Server Error";
+                    status = HttpStatusCode.InternalServerError;
+                }
+                var statusCode = Convert.ToInt32(status);
+                var responseData = new
+                {
+                    ErrorCode = statusCode,
+                    ErrorMsg = message
+                };
+                context.Result = new ObjectResult(responseData)
+                {
+                    StatusCode = statusCode
+                };
+                context.ExceptionHandled = true; 
+            }
+        }
+    }
+}
